542e5e569f14a1312637d21b92de89177529435d
itslitulinchpin2 <itslitulinchpin@gmail.com>
2023-10-14 12:47:51 +0200

    initial setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+//개발전체과정 정리
+//1. 왼쪽엔 연락처 등록폼, 오른쪽엔 연락처와 검색창
+//2. 이름과 번호를 입력하면 연락처에 추가
+//3. 리스트의 사람 수가 보임
+//4. 이름으로 연락처 검색 가능
+// 리덕스를 사용한다!
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Redux PhoneBook Application!
     </div>
   );
 }
